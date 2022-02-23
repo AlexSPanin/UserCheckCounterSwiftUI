ab5e33554c08e4fd91bf9cc8aad78c625fa36bab
Alexander Panin <panin1970@gmail.com>
2022-02-23 12:43:36 +0300

    Initial Commit

---
+++ UserCheckCounterSwiftUI/UserCheckCounterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserCheckCounterSwiftUIApp.swift
+//  UserCheckCounterSwiftUI
+//
+//  Created by Александр Панин on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct UserCheckCounterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
